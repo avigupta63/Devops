81b57246f4aaca47a5b00f1d6e8f3eee6bdac7ac
Anil bhau <anil@gmail.com>
2025-02-04 17:50:41 +0530

    tf-notes-added

---
+++ terraform/ec2-alb-as/var.tf
@@ -0,0 +1,23 @@
+variable "image_id" {
+    default = "ami-012967cc5a8c9f891"
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "key_pair" {
+    default = "new-key-nv"
+}
+
+variable "subnet_id_1" {
+    default = "subnet-050cc6904df99c2a8"
+}
+
+variable "subnet_id_2" {
+    default = "subnet-091678d44aa5db41d"
+}
+
+variable "vpc_id" {
+    default = "vpc-07a98d6f3268517fb"
+}
